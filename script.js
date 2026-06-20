8f6f1525b0f56597a0350334881af26dbd1766e3
JustAGuest <rich.cat.0001@gmail.com>
2026-06-20 15:07:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const frame = document.getElementById("contentFrame")
-
 const buttons = document.querySelectorAll(".tabBtn")
+const menuBtn = document.getElementById("menuBtn")
+const sidebar = document.getElementById("sidebar")
 
 buttons.forEach(button => {
 
@@ -12,7 +13,21 @@ buttons.forEach(button => {
 
         button.classList.add("active")
 
-        frame.src = button.dataset.tab
+        frame.style.opacity = "0"
+
+        setTimeout(() => {
+            frame.src = button.dataset.tab
+            frame.style.opacity = "1"
+        }, 150)
+
+        if (window.innerWidth <= 768) {
+            sidebar.classList.remove("open")
+        }
+
     })
 
+})
+
+menuBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("open")
 })
