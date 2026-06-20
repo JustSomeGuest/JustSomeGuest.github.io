2429561b48f9635f683d404cbdeeeea329820a1d
JustAGuest <rich.cat.0001@gmail.com>
2026-06-20 15:13:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,32 +2,36 @@ const frame = document.getElementById("contentFrame")
 const buttons = document.querySelectorAll(".tabBtn")
 const menuBtn = document.getElementById("menuBtn")
 const sidebar = document.getElementById("sidebar")
+const overlay = document.getElementById("overlay")
 
 buttons.forEach(button => {
 
-    button.addEventListener("click", () => {
+    button.onclick = () => {
 
-        buttons.forEach(btn => {
-            btn.classList.remove("active")
-        })
-
+        buttons.forEach(btn => btn.classList.remove("active"))
         button.classList.add("active")
 
-        frame.style.opacity = "0"
-
-        setTimeout(() => {
-            frame.src = button.dataset.tab
-            frame.style.opacity = "1"
-        }, 150)
+        frame.src = button.dataset.tab
 
         if (window.innerWidth <= 768) {
             sidebar.classList.remove("open")
+            overlay.classList.remove("open")
         }
 
-    })
+    }
 
 })
 
-menuBtn.addEventListener("click", () => {
+menuBtn.onclick = () => {
+
     sidebar.classList.toggle("open")
-})
+    overlay.classList.toggle("open")
+
+}
+
+overlay.onclick = () => {
+
+    sidebar.classList.remove("open")
+    overlay.classList.remove("open")
+
+}
